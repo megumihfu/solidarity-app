78a38ccf667c2e5597dac11e521c92efe4a9a348
megumi hfu <daffef@et.esiea.fr>
2026-01-22 21:31:49 +0100

    first commit: init spring boot backend structure

---
+++ back/src/main/java/com/example/solidarityapp/SolidarityappApplication.java
@@ -0,0 +1,13 @@
+package com.example.solidarityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidarityappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidarityappApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/solidarityapp/SolidarityappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solidarityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidarityappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
