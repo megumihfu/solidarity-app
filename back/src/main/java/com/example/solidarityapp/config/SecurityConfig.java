758bc8d6c35f5fd30228134c31df4a990bd93dde
megumi hfu <daffef@et.esiea.fr>
2026-07-17 20:30:07 +0200

    feat: added cors configuration

--- back/src/main/java/com/example/solidarityapp/config/SecurityConfig.java
+++ back/src/main/java/com/example/solidarityapp/config/SecurityConfig.java
@@ -3,13 +3,24 @@ package com.example.solidarityapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${spring.app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -23,5 +34,20 @@ public class SecurityConfig {
                 .anyRequest().permitAll() //@todo JWT auth
             );
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration conf = new CorsConfiguration();
+        conf.setAllowedOrigins(allowedOrigins);
+        conf.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        conf.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        conf.setExposedHeaders(List.of("Authorization"));
+        conf.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", conf);
+
+        return source;
     }
 }
--- back/src/main/java/com/example/solidarityapp/config/WebConfig.java
+++ back/src/main/java/com/example/solidarityapp/config/WebConfig.java
@@ -8,9 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost")
                 .allowedOrigins("http://127.0.0.1:3000")
                 .allowedOrigins("http://127.0.0.1:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
